ed45dd1eee87c92a3b1964b7d9a06c23c3cf408b
IchigoMazone <nhattrinh2702@gmail.com>
2026-07-23 21:00:21 +0700

    bump: version 2.0.3

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="klygo",
-    version="2.0.2",
+    version="2.0.3",
     packages=find_packages(exclude=["cpp*", "test*", "build*", "_cmake_build*"]),
     distclass=BinaryDistribution,
     cmdclass={"build_ext": CMakeBuild},
